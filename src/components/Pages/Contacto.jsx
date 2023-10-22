915461a8e865c273018492d9b44db0611a5a5cd1
agustin <marceloagustingomezaranda@gmail.com>
2023-10-22 20:15:18 -0300

    terminando de corregir los textos de contacto

--- src/components/Pages/Contacto.jsx
+++ src/components/Pages/Contacto.jsx
@@ -13,7 +13,8 @@ const Contacto = () => {
           </h1>
 
           <p className="mt-3 text-gray-500 dark:text-gray-400">
-            We’d love to hear from you. Please fill out this form or shoot us an email.
+            
+Nos encantaría saber cual es su consulta. Por favor complete este formulario o envíenos un correo electrónico.
           </p>
         </div>
 
@@ -27,8 +28,9 @@ const Contacto = () => {
               </span>
 
               <h2 className="mt-4 text-base font-medium text-gray-800 dark:text-white">Email</h2>
-              <p className="mt-2 text-sm text-gray-500 dark:text-gray-400">Our friendly team is here to help.</p>
-              <p className="mt-2 text-sm text-blue-500 dark:text-blue-400">hello@merakiui.com</p>
+              <p className="mt-2 text-sm text-gray-500 dark:text-gray-400">
+Nuestro equipo está aquí para ayudarlo.</p>
+              <p className="mt-2 text-sm text-blue-500 dark:text-blue-400">consulta@digital-mirage.com</p>
             </div>
 
             <div>
@@ -39,9 +41,8 @@ const Contacto = () => {
                 </svg>
               </span>
 
-              <h2 className="mt-4 text-base font-medium text-gray-800 dark:text-white">Live chat</h2>
-              <p className="mt-2 text-sm text-gray-500 dark:text-gray-400">Our friendly team is here to help.</p>
-              <p className="mt-2 text-sm text-blue-500 dark:text-blue-400">Start new chat</p>
+              <h2 className="mt-4 text-base font-medium text-gray-800 dark:text-white">24 hs Activos</h2>
+              <p className="mt-2 text-sm text-gray-500 dark:text-gray-400">Podes contactarnos las 24 hs asi resolvemos tu inconveniente</p>
             </div>
 
             <div>
@@ -52,8 +53,8 @@ const Contacto = () => {
                 </svg>
               </span>
 
-              <h2 className="mt-4 text-base font-medium text-gray-800 dark:text-white">Office</h2>
-              <p className="mt-2 text-sm text-gray-500 dark:text-gray-400">Come say hello at our office HQ.</p>
+              <h2 className="mt-4 text-base font-medium text-gray-800 dark:text-white">Oficina</h2>
+              <p className="mt-2 text-sm text-gray-500 dark:text-gray-400">Conoce nuestras oficinas</p>
               <p className="mt-2 text-sm text-blue-500 dark:text-blue-400">100 Smith Street Collingwood VIC 3066 AU</p>
             </div>
 
@@ -64,8 +65,8 @@ const Contacto = () => {
                 </svg>
               </span>
 
-              <h2 className="mt-4 text-base font-medium text-gray-800 dark:text-white">Phone</h2>
-              <p className="mt-2 text-sm text-gray-500 dark:text-gray-400">Mon-Fri from 8am to 5pm.</p>
+              <h2 className="mt-4 text-base font-medium text-gray-800 dark:text-white">Teléfono</h2>
+              <p className="mt-2 text-sm text-gray-500 dark:text-gray-400">Contactanos via Whatsapp</p>
               <p className="mt-2 text-sm text-blue-500 dark:text-blue-400">+1 (555) 000-0000</p>
             </div>
           </div>
@@ -74,7 +75,7 @@ const Contacto = () => {
             <form>
               <div className="-mx-2 md:items-center md:flex">
                 <div className="flex-1 px-2">
-                  <label className="block mb-2 text-sm text-gray-600 dark:text-gray-200">First Name</label>
+                  <label className="block mb-2 text-sm text-gray-600 dark:text-gray-200">Nombre</label>
                   <input
                     type="text"
                     placeholder="John"
@@ -83,7 +84,7 @@ const Contacto = () => {
                 </div>
 
                 <div className="flex-1 px-2 mt-4 md:mt-0">
-                  <label className="block mb-2 text-sm text-gray-600 dark:text-gray-200">Last Name</label>
+                  <label className="block mb-2 text-sm text-gray-600 dark:text-gray-200">Apellido</label>
                   <input
                     type="text"
                     placeholder="Doe"
@@ -93,7 +94,7 @@ const Contacto = () => {
               </div>
 
               <div className="mt-4">
-                <label className="block mb-2 text-sm text-gray-600 dark:text-gray-200">Email address</label>
+                <label className="block mb-2 text-sm text-gray-600 dark:text-gray-200">marceloagustingomezaranda@gmail.com</label>
                 <input
                   type="email"
                   placeholder="johndoe@example.com"
@@ -102,7 +103,7 @@ const Contacto = () => {
               </div>
 
               <div className="w-full mt-4">
-                <label className="block mb-2 text-sm text-gray-600 dark:text-gray-200">Message</label>
+                <label className="block mb-2 text-sm text-gray-600 dark:text-gray-200">Mensaje</label>
                 <textarea
                   className="block w-full h-32 px-5 py-2.5 mt-2 text-gray-700 placeholder-gray-400 bg-white border border-gray-200 rounded-lg md:h-56 dark:placeholder-gray-600 dark:bg-gray-900 dark:text-gray-300 dark:border-gray-700 focus:border-blue-400 dark:focus:border-blue-400 focus:ring-blue-400 focus:outline-none focus:ring focus:ring-opacity-40"
                   placeholder="Message"
