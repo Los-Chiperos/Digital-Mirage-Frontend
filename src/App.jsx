ed847c1c29f8673fa16f584f6ec43d6592370679
Nicolas <nicolaszelarayan.ez@gmail.com>
2023-10-24 14:09:29 -0300

    add rout

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,12 @@
 import React, { useState, useEffect } from 'react';
-import LoadingScreen from "./components/Fragments/LoadingScreen.jsx";
-import Header from "./components/layout/Header.jsx";
-import Footer from "./components/layout/Footer.jsx";
-import Banner2 from "./components/Fragments/Banner2.jsx";
-import Banner from "./components/Fragments/Banner.jsx";
-import { BrowserRouter } from "react-router-dom";
-import Router from "./Router";
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import LoadingScreen from './components/Fragments/LoadingScreen.jsx';
+import Header from './components/layout/Header.jsx';
+import Footer from './components/layout/Footer.jsx';
+import Banner2 from './components/Fragments/Banner2.jsx';
+import Banner from './components/Fragments/Banner.jsx';
+import Nosotros from './components/Pages/Nosotros.jsx';
+import Router from './Router';
 
 function App() {
   const [isLoading, setIsLoading] = useState(true);
@@ -13,20 +14,22 @@ function App() {
   useEffect(() => {
     setTimeout(() => {
       setIsLoading(false);
-    }, 1500); // Oculta la pantalla de carga después de 2.5 segundos
+    }, 1500); // Oculta la pantalla de carga después de 1.5 segundos
   }, []);
 
   return (
     <BrowserRouter>
-    {isLoading ? (
-      <LoadingScreen /> // Muestra la pantalla de carga mientras isLoading sea true
+      {isLoading ? (
+        <LoadingScreen /> // Muestra la pantalla de carga mientras isLoading sea true
       ) : (
-      <div>
-      
+        <div>
           <Header />
           <Banner />
           <main>
-            <Router />
+            <Routes>
+              <Route path="/nosotros" element={<Nosotros />} />
+              {/* Otras rutas pueden ser definidas aquí */}
+            </Routes>
           </main>
           <Banner2 />
           <Footer />
@@ -37,3 +40,4 @@ function App() {
 }
 
 export default App;
+
