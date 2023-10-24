d10f3a1e28cf94c98ab2c4792c3598c5e2e733a8
agustin <marceloagustingomezaranda@gmail.com>
2023-10-24 19:22:00 -0300

    agregnado location para que no aparezca los 2 banners en faq y contacto

--- src/App.jsx
+++ src/App.jsx
@@ -6,7 +6,6 @@ import Footer from './components/layout/Footer.jsx';
 import Banner2 from './components/Fragments/Banner2.jsx';
 import Banner from './components/Fragments/Banner.jsx';
 import Router from './Router';
-import Faq from './components/Pages/SubPages/Faq.jsx';
 
 function App() {
   const [isLoading, setIsLoading] = useState(true);
@@ -28,8 +27,9 @@ function App() {
           {window.location.pathname !== '/faq' && window.location.pathname !== '/contacto' && <Banner />}
           <main>
             <Router />
-          </main>
-          <Banner2 />
+            </main>
+          {/* Condicional para mostrar o no el Banner2 en función de la ruta */}
+          {window.location.pathname !== '/faq' && window.location.pathname !== '/contacto' && <Banner2 />}
           <Footer />
         </div>
       )}
