f5e1c6a98edb8d82d7b9702ee8f1e535a0818971
Fernando Nicolas Zelarayan <98678489+NicolasZelarayan@users.noreply.github.com>
2023-10-28 00:16:31 -0300

    Update ProductCard.jsx

--- src/components/Fragments/ProductCard.jsx
+++ src/components/Fragments/ProductCard.jsx
@@ -17,20 +17,21 @@ function ProductCard({ product }) {
         <p className="text-sm text-gray-500 dark:text-gray-400 mb-2">{marca}</p>
         <div className="flex items-center mt-2.5 mb-5">
           <div>
-            {[1, 2, 3, 4, 5].map((star) => (
-              <svg
-                key={star}
-                className={`w-4 h-4 ${
-                  star <= descripcion.rating ? 'text-yellow-300' : 'text-gray-200 dark:text-gray-600'
-                } mr-1`}
-                aria-hidden="true"
-                xmlns="http://www.w3.org/2000/svg"
-                fill="currentColor"
-                viewBox="0 0 22 20"
-              >
-                <path d="M20.924 7.625a1.523 1.523 0 0 0-1.238-1.044l-5.051-.734-2.259-4.577a1.534 1.534 0 0 0-2.752 0L7.365 5.847l-5.051.734A1.535 1.535 0 0 0 1.463 9.2l3.656 3.563-.863 5.031a1.532 1.532 0 0 0 2.226 1.616L11 17.033l4.518 2.375a1.534 1.534 0 0 0 2.226-1.617l-.863-5.03L20.537 9.2a1.523 1.523 0 0 0 .387-1.575Z" />
-              </svg>
-      })
+{[1, 2, 3, 4, 5].map((star) => (
+  <svg
+    key={star}
+    className={`w-4 h-4 ${
+      star <= descripcion.rating ? 'text-yellow-300' : 'text-gray-200 dark:text-gray-600'
+    } mr-1`}
+    aria-hidden="true"
+    xmlns="http://www.w3.org/2000/svg"
+    fill="currentColor"
+    viewBox="0 0 22 20"
+  >
+    <path d="M20.924 7.625a1.523 1.523 0 0 0-1.238-1.044l-5.051-.734-2.259-4.577a1.534 1.534 0 0 0-2.752 0L7.365 5.847l-5.051.734A1.535 1.535 0 0 0 1.463 9.2l3.656 3.563-.863 5.031a1.532 1.532 0 0 0 2.226 1.616L11 17.033l4.518 2.375a1.534 1.534 0 0 0 2.226-1.617l-.863-5.03L20.537 9.2a1.523 1.523 0 0 0 .387-1.575Z" />
+  </svg>
+))}
+
           </div>
           <span className="bg-blue-100 text-blue-800 text-xs font-semibold mr-2 px-2.5 py-0.5 rounded dark:bg-blue-200 dark:text-blue-800 ml-3">
             {descripcion.rating}
