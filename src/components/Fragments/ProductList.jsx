53375827697847751361ebb7796ded902a7da325
Fernando Nicolas Zelarayan <98678489+NicolasZelarayan@users.noreply.github.com>
2023-10-28 00:22:39 -0300

    Update ProductList.jsx

--- src/components/Fragments/ProductList.jsx
+++ src/components/Fragments/ProductList.jsx
@@ -2,17 +2,15 @@ import React, { useState, useEffect } from 'react';
 import ProductCard from "./ProductCard";
 
 function ProductList({ products }) {
-
   return (
     <>
       <div className="flex flex-col items-center gap-6">
         <h2 className="text-2xl font-semibold text-gray-900 dark:text-white">Productos destacados</h2>
         <div className="flex justify-center flex-wrap gap-4">
-
           {
             products.map((product, idx) => {
               return (
-                <ProductCard key={idx} products={product} />
+                <ProductCard key={idx} product={product} /> {/* Cambia 'products' a 'product' */
               )
             })
           }
