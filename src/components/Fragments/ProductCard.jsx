2613caf4aa119545fa1e60bbc7d522e25b3563ac
Agustin Gomez Aranda <marceloagustingomezaranda@gmail.com>
2023-11-04 01:49:16 -0300

    acomodando las imagenes del producto

--- src/components/Fragments/ProductCard.jsx
+++ src/components/Fragments/ProductCard.jsx
@@ -63,7 +63,8 @@ const starColor = (starIndex) => {
     <div className="w-full h-full max-w-sm bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
 
       <a href="#">
-        <img className="p-8 rounded-t-lg" src={url_image} alt="product image" style={imgEstilo}/>
+      <img className="p-8 rounded-t-lg object-contain" src={url_image} alt="product image" style={imgEstilo} />
+
       </a>
       <div className="px-5 pb-5">
         <a href="#">
