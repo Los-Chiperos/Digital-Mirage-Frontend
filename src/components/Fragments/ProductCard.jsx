4e81822aa2113628a5b5c39dee2cb9dce4f3a983
Agustin Gomez Aranda <marceloagustingomezaranda@gmail.com>
2023-11-04 05:56:39 -0300

    llevando los botones de agregar y quitar abajo del precio

--- src/components/Fragments/ProductCard.jsx
+++ src/components/Fragments/ProductCard.jsx
@@ -12,12 +12,9 @@ function formatPrice(price) {
 function ProductCard({ _id, modelo, marca, descripcion, precio, url_image, onProductClick, darkMode }) {
     const [cart, setCart] = useContext(CartContext);
 
-    // Genera una valoración aleatoria en el rango de 2.5 a 5
     const randomRating = (Math.random() * (5 - 2.5) + 2.5).toFixed(1);
-    // Redondea la valoración a la cantidad de estrellas enteras
     const rating = Math.round(randomRating);
 
-    // Función para determinar el color de la estrella (dorada o gris)
     const starColor = (starIndex) => {
         return starIndex <= rating ? 'text-yellow-300' : 'text-gray-200 dark:text-gray-600';
     };
@@ -79,52 +76,35 @@ function ProductCard({ _id, modelo, marca, descripcion, precio, url_image, onPro
                 </a>
                 <p className={`text-sm ${darkMode ? 'text-gray-500' : 'text-gray-400'} mb-2`}>{marca}</p>
                 <p className={`text-sm ${darkMode ? 'text-gray-500' : 'text-gray-400'} mb-2`}>{descripcion}</p>
-                <div className="flex items-center mt-2.5 mb-5">
-                    <div style={{ display: 'flex' }}>
-                        {[1, 2, 3, 4, 5].map((star) => (
-                            <svg
-                                key={star}
-                                className={`w-4 h-4 ${starColor(star)}`}
-                                aria-hidden="true"
-                                xmlns="http://www.w3.org/2000/svg"
-                                fill="currentColor"
-                                viewBox="0 0 22 20"
-                            >
-                                <path d="M20.924 7.625a1.523 1.523 0 0 0-1.238-1.044l-5.051-.734-2.259-4.577a1.534 1.534 0 0 0-2.752 0L7.365 5.847l-5.051.734A1.535 1.535 0 0 0 1.463 9.2l3.656 3.563-.863 5.031a1.532 1.532 0 0 0 2.226 1.616L11 17.033l4.518 2.375a1.534 1.534 0 0 0 2.226-1.617l-.863-5.03L20.537 9.2a1.523 1.523 0 0 0 .387-1.575Z" />
-                            </svg>
-                        ))}
-                    </div>
-                    <span className={`bg-blue-100 text-blue-800 text-xs font-semibold mr-2 px-2.5 py-0.5 rounded ${darkMode ? 'dark:bg-blue-200 dark:text-blue-800' : 'bg-blue-100 text-blue-800'} ml-3`}>
-                        {rating}
-                    </span>
-                </div>
-                <div className="flex items-center justify-between">
+                <div className="mt-2.5 mb-5">
                     <span className={`text-3xl font-bold ${darkMode ? 'text-gray-900' : 'dark:text-white'}`}>
                         {formatPrice(precio)}
                     </span>
-                    {quantityPerItem === 0 ? (
-                        <button
-                            className={`text-white bg-blue-700 hover-bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center ${darkMode ? 'dark:bg-blue-600 dark:hover-bg-blue-700 dark:focus:ring-blue-800' : 'bg-blue-700 hover-bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover-bg-blue-700 dark:focus:ring-blue-800'}`}
-                            onClick={() => addToCart()}
-                        >
-                            Agregar
-                        </button>
-                    ) : (
-                        <>
+                    <div className="mt-2">
+                        {quantityPerItem === 0 ? (
                             <button
                                 className={`text-white bg-blue-700 hover-bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center ${darkMode ? 'dark:bg-blue-600 dark:hover-bg-blue-700 dark:focus:ring-blue-800' : 'bg-blue-700 hover-bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover-bg-blue-700 dark:focus:ring-blue-800'}`}
                                 onClick={() => addToCart()}
                             >
                                 Agregar
                             </button>
-                            <button
-                                className={`text-white bg-red-700 hover-bg-red-700 focus-ring-4 focus:outline-none focus:ring-red-400 font-medium rounded-lg text-sm px-5 py-2.5 text-center ${darkMode ? 'dark:bg-red-600 dark:hover-bg-red-700 dark:focus-ring-red-900' : 'bg-red-700 hover-bg-red-700 focus-ring-4 focus:outline-none focus:ring-red-400 dark:bg-red-600 dark:hover-bg-red-700 dark:focus-ring-red-900'}`}
-                                onClick={() => removeItem(_id)}
-                            >
-                                Quitar
-                            </button>
-                        </>
-                    )}
+                        ) : (
+                            <div className="flex justify-between">
+                                <button
+                                    className={`text-white bg-blue-700 hover-bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center ${darkMode ? 'dark:bg-blue-600 dark:hover-bg-blue-700 dark:focus:ring-blue-800' : 'bg-blue-700 hover-bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover-bg-blue-700 dark:focus:ring-blue-800'}`}
+                                    onClick={() => addToCart()}
+                                >
+                                    Agregar
+                                </button>
+                                <button
+                                    className={`text-white bg-red-700 hover-bg-red-700 focus-ring-4 focus:outline-none focus:ring-red-400 font-medium rounded-lg text-sm px-5 py-2.5 text-center ${darkMode ? 'dark:bg-red-600 dark:hover-bg-red-700 dark:focus-ring-red-900' : 'bg-red-700 hover-bg-red-700 focus-ring-4 focus:outline-none focus:ring-red-400 dark:bg-red-600 dark:hover-bg-red-700 dark:focus-ring-red-900'}`}
+                                    onClick={() => removeItem(_id)}
+                                >
+                                    Quitar
+                                </button>
+                            </div>
+                        )}
+                    </div>
                 </div>
             </div>
         </div>
