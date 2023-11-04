c123b9a9a40e0d317a59595c62219f00a4b7c688
Agustin Gomez Aranda <marceloagustingomezaranda@gmail.com>
2023-11-04 05:05:47 -0300

    agregando funcionalidad dark mode

--- src/components/layout/Header.jsx
+++ src/components/layout/Header.jsx
@@ -2,17 +2,24 @@ import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
 import Registro from '../Fragments/Registro.jsx';
 import Login from '../Fragments/Login.jsx';
+import { Switch } from '@headlessui/react';
 
 const Header = () => {
     const [menuOpen, setMenuOpen] = useState(false);
+    const [darkMode, setDarkMode] = useState(false);
 
     const toggleMenu = () => {
         setMenuOpen(!menuOpen);
     };
 
+    const toggleDarkMode = () => {
+        setDarkMode(!darkMode);
+        // Agrega aquí la lógica para cambiar el tema de tu aplicación a modo oscuro o claro.
+    };
+
     return (
         <div>
-            <nav className="bg-white dark:bg-gray-900 fixed w-full z-20 top-0 left-0 border-b border-gray-200 dark:border-gray-600">
+            <nav className={`bg-white ${darkMode ? 'dark:bg-gray-900' : 'dark:bg-gray-50'} fixed w-full z-20 top-0 left-0 border-b ${darkMode ? 'dark:border-gray-600' : 'border-gray-200'}`}>
                 <div className="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
                     <Link to="http://digital-mirage.ar/" className="flex items-center">
                         <img
@@ -21,6 +28,23 @@ const Header = () => {
                             alt="Digital Mirage Logo"
                         />
                     </Link>
+
+                    {/* Agregar el interruptor de modo oscuro aquí */}
+                    <Switch
+                        checked={darkMode}
+                        onChange={toggleDarkMode}
+                        className={`${
+                            darkMode ? 'bg-gray-700' : 'bg-gray-200'
+                        } relative inline-flex items-center h-6 rounded-full w-11 transition-colors focus:outline-none focus:ring-2 focus:ring-gray-200 dark:focus:ring-gray-700`}
+                    >
+                        <span className="sr-only">Modo oscuro</span>
+                        <span
+                            className={`${
+                                darkMode ? 'translate-x-6' : 'translate-x-1'
+                            } inline-block w-4 h-4 transform bg-white rounded-full transition-transform`}
+                        />
+                    </Switch>
+
                     <div className="flex md:order-2">
                         <Login />
                         <Registro />
@@ -47,45 +71,46 @@ const Header = () => {
                             </svg>
                         </button>
                     </div>
+
                     <div
                         className={`items-center justify-between w-full md:flex md:w-auto md:order-1 ${menuOpen ? 'block' : 'hidden'}`}
                         id="navbar-sticky"
                     >
-                        <ul className="flex flex-col p-4 md:p-0 mt-4 font-medium border border-gray-100 rounded-lg bg-gray-50 md:flex-row md:space-x-8 md:mt-0 md:border-0 md:bg-white dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700">
-                            <li>
-                                <Link
-                                    to="/"
-                                    className="block py-2 pl-3 pr-4 text-white bg-blue-700 rounded md:bg-transparent md:text-blue-700 md:p-0 md:dark:text-blue-500"
-                                    aria-current="page"
-                                >
-                                    Inicio
-                                </Link>
-                            </li>
-                            <li>
-                                <Link
-                                    to="/acercade"
-                                    className="block py-2 pl-3 pr-4 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 md:dark:hover:text-blue-500 dark:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700"
-                                >
-                                    Nosotros
-                                </Link>
-                            </li>
-                        
-                            <li>
-                                <Link
-                                    to="/servicios"
-                                    className="block py-2 pl-3 pr-4 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 md:dark:hover:text-blue-500 dark:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700"
-                                >
-                                    Servicios
-                                </Link>
-                            </li>
-                            <li>
-                                <Link
-                                    to="/contacto"
-                                    className="block py-2 pl-3 pr-4 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 md:dark:hover:text-blue-500 dark:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700"
-                                >
-                                    Contacto
-                                </Link>
-                            </li>
+                        <ul className="flex flex-col p-4 md:p-0 mt-4 font-medium border border-gray-100 rounded-lg bg-white-100 md:flex-row md:space-x-8 md:mt-0 md:border-0 md:bg-white dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700">
+                        <li>
+    <Link
+        to="/"
+        className={`block py-2 pl-3 pr-4 ${darkMode ? 'text-blue-500 dark:text-white' : 'text-white'} ${darkMode ? 'bg-gray-700 dark:bg-gray-800' : 'bg-blue-700'} rounded md:bg-transparent md:text-blue-700 md:p-0`}
+        aria-current="page"
+    >
+        Inicio
+    </Link>
+</li>
+<li>
+    <Link
+        to="/acercade"
+        className={`block py-2 pl-3 pr-4 ${darkMode ? 'text-blue-500 dark:text-white' : 'text-gray-900'} ${darkMode ? 'hover:bg-gray-800 dark:hover:bg-gray-700' : 'hover:bg-gray-100'} md:hover:bg-transparent md:hover:text-blue-700 md:p-0`}
+    >
+        Nosotros
+    </Link>
+</li>
+<li>
+    <Link
+        to="/servicios"
+        className={`block py-2 pl-3 pr-4 ${darkMode ? 'text-blue-500 dark:text-white' : 'text-gray-900'} ${darkMode ? 'hover:bg-gray-800 dark:hover:bg-gray-700' : 'hover:bg-gray-100'} md:hover:text-blue-700 md:p-0`}
+    >
+        Servicios
+    </Link>
+</li>
+<li>
+    <Link
+        to="/contacto"
+        className={`block py-2 pl-3 pr-4 ${darkMode ? 'text-blue-500 dark:text-white' : 'text-gray-900'} ${darkMode ? 'hover:bg-gray-800 dark:hover:bg-gray-700' : 'hover:bg-gray-100'} md:hover:text-blue-700 md:p-0`}
+    >
+        Contacto
+    </Link>
+</li>
+
                         </ul>
                     </div>
                 </div>
