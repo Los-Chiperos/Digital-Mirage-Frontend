a79752e0880dfcd05830259e6cb18ed2e027dc1a
Agustin Gomez Aranda <marceloagustingomezaranda@gmail.com>
2023-11-04 02:41:14 -0300

    arreglando menu de hamburgesa

--- src/components/layout/Header.jsx
+++ src/components/layout/Header.jsx
@@ -1,12 +1,16 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
 import Registro from '../Fragments/Registro.jsx';
 import Login from '../Fragments/Login.jsx';
 
-
 const Header = () => {
-    return (
+    const [menuOpen, setMenuOpen] = useState(false);
 
+    const toggleMenu = () => {
+        setMenuOpen(!menuOpen);
+    };
+
+    return (
         <div>
             <nav className="bg-white dark:bg-gray-900 fixed w-full z-20 top-0 left-0 border-b border-gray-200 dark:border-gray-600">
                 <div className="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
@@ -21,11 +25,9 @@ const Header = () => {
                         <Login />
                         <Registro />
                         <button
-                            data-collapse-toggle="navbar-sticky"
+                            onClick={toggleMenu}
                             type="button"
                             className="inline-flex items-center p-2 w-10 h-10 justify-center text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600"
-                            aria-controls="navbar-sticky"
-                            aria-expanded="false"
                         >
                             <span className="sr-only">Abrir menú principal</span>
                             <svg
@@ -46,7 +48,7 @@ const Header = () => {
                         </button>
                     </div>
                     <div
-                        className="items-center justify-between hidden w-full md:flex md:w-auto md:order-1"
+                        className={`items-center justify-between w-full md:flex md:w-auto md:order-1 ${menuOpen ? 'block' : 'hidden'}`}
                         id="navbar-sticky"
                     >
                         <ul className="flex flex-col p-4 md:p-0 mt-4 font-medium border border-gray-100 rounded-lg bg-gray-50 md:flex-row md:space-x-8 md:mt-0 md:border-0 md:bg-white dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700">
