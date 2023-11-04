f83d370055a69ef3c2e485c92cc109ef7f67cfeb
Agustin Gomez Aranda <marceloagustingomezaranda@gmail.com>
2023-11-04 03:19:00 -0300

    modificando estilos del product detail

--- src/components/Fragments/ProductDetail.jsx
+++ src/components/Fragments/ProductDetail.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react";
+import React, { useState, useEffect } from "react";
 import { useParams } from "react-router-dom";
 import axios from "axios";
 
@@ -29,21 +29,53 @@ function ProductDetail() {
   }
 
   if (!product) {
-    return null; // o un mensaje de error personalizado
+    return <p>Producto no encontrado</p>;
   }
 
   return (
-    <div>
-      <h1>{product.modelo}</h1>
-      <p>Marca: {product.marca}</p>
-      <p>Descripción: {product.descripcion}</p>
-      <p>Stock: {product.stock}</p>
-      <p>Precio: {product.precio}</p>
-      <img src={product.url_image} alt={product.modelo} />
-
-      {/* Agrega más detalles del producto según tu estructura de datos */}
-    </div>
+    <section className="text-gray-700 body-font overflow-hidden bg-white">
+      <div className="container px-5 py-24 mx-auto">
+        <div className="lg:w-4/5 mx-auto flex flex-wrap">
+          <img
+            alt={product.modelo}
+            className="lg:w-1/2 w-full object-cover object-center rounded border border-gray-200"
+            src={product.url_image}
+          />
+          <div className="lg:w-1/2 w-full lg:pl-10 lg:py-6 mt-6 lg:mt-0">
+            <h2 className="text-sm title-font text-gray-500 tracking-widest">
+              {product.marca}
+            </h2>
+            <h1 className="text-gray-900 text-3xl title-font font-medium mb-1">
+              {product.modelo}
+            </h1>
+            <p className="leading-relaxed">
+              {product.descripcion}
+            </p>
+            <div className="flex mt-6 items-center pb-5 border-b-2 border-gray-200 mb-5">
+              <div className="flex">
+                
+              </div>
+              <div className="flex ml-6 items-center">
+               
+                <div className="relative">
+                  
+                
+                </div>
+              </div>
+            </div>
+            <div className="flex">
+              <span className="title-font font-medium text-2xl text-gray-900">${product.precio}</span>
+              <button className="flex ml-auto text-white bg-red-500 border-0 py-2 px-6 focus:outline-none hover-bg-red-600 rounded">
+                Agregar
+              </button>
+           
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
   );
 }
 
 export default ProductDetail;
+
