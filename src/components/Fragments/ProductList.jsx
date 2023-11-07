adf7a2c375372bce39e7677f8d028f4d5834f987
Oscar Gonzalo Araya <gonxaraya991@gmail.com>
2023-11-07 19:29:59 -0300

    se agrego paginacion a los productos

--- src/components/Fragments/ProductList.jsx
+++ src/components/Fragments/ProductList.jsx
@@ -1,13 +1,44 @@
-import React from 'react';
+import React, { useState } from 'react';
 import ProductCard from "./ProductCard";
+import  Pagination from './Pagination';
 
 function ProductList({ products, onProductClick }) {
+
+  //    Paginación
+  const productosPorPagina = 15;
+  const totalDePaginas = Math.ceil(products.length / productosPorPagina);
+  const [currentPage, setCurrentPage] = useState(1);
+  // Calcula el índice inicial y final de las noticias a mostrar en la página actual
+  const startIndex = (currentPage - 1) * productosPorPagina;
+  const endIndex = startIndex + productosPorPagina - 1;
+
+  const productosActuales = products.slice(startIndex, endIndex + 1);
+
+  //Función para cambiar de página
+  const handleGoToPage = (pageNumber) => {
+    setCurrentPage(pageNumber);
+  };
+
+  // Función para manejar el evento del botón "Anterior"
+  const handlePreviousPage = () => {
+    if (currentPage > 1) {
+      setCurrentPage(currentPage - 1);
+    }
+  };
+
+  // Función para manejar el evento del botón "Siguiente"
+  const handleNextPage = () => {
+    if (currentPage < totalDePaginas) {
+      setCurrentPage(currentPage + 1);
+    }
+  };
+
   return (
     <>
       <div className="flex flex-col items-center gap-6 mb-5">
         <h2 className="text-2xl font-semibold text-gray-900 dark:text-black">Productos destacados</h2>
         <div className="flex justify-center flex-wrap gap-4">
-          {products.map((product) => (
+          {productosActuales.map((product) => (
             <ProductCard
               key={product._id}
               onProductClick={() => onProductClick(product._id)}
@@ -15,6 +46,9 @@ function ProductList({ products, onProductClick }) {
             />
           ))}
         </div>
+        <Pagination totalDePaginas={totalDePaginas}
+          currentPage={currentPage} handleGoToPage={handleGoToPage}
+          handlePreviousPage={handlePreviousPage} handleNextPage={handleNextPage} />
       </div>
     </>
   );
