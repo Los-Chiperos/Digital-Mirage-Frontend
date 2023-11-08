e2aee4b2406c7c16f89bd2bc55f2bf08c58d0826
= <nicolaszelarayan.ez@gmail.com>
2023-11-08 16:44:36 -0300

    fix header

--- src/components/layout/Header.jsx
+++ src/components/layout/Header.jsx
@@ -2,23 +2,15 @@ import React, { useContext, useState } from 'react';
 import { Link } from 'react-router-dom';
 import Registro from '../Fragments/Registro.jsx';
 import Login from '../Fragments/Login.jsx';
-import { Switch } from '@headlessui/react';
 import { LoginContext } from '../Context/LoginContext.jsx';
 import "../Css/filter.css"
-
 
 const Header = () => {
     const [login, setLogin] = useContext(LoginContext);
     const [menuOpen, setMenuOpen] = useState(false);
-    const [darkMode, setDarkMode] = useState(false);
 
     const toggleMenu = () => {
         setMenuOpen(!menuOpen);
-    };
-
-    const toggleDarkMode = () => {
-        setDarkMode(!darkMode);
-        // Agrega aquí la lógica para cambiar el tema de tu aplicación a modo oscuro o claro.
     };
 
     const handleLogout = () => {
@@ -27,7 +19,7 @@ const Header = () => {
 
     return (
         <div>
-            <nav className={`bg-white ${darkMode ? 'dark:bg-gray-900' : 'dark:bg-gray-50'} fixed w-full z-20 top-0 left-0 border-b ${darkMode ? 'dark:border-gray-600' : 'border-gray-200'}`}>
+            <nav className="bg-black fixed w-full z-20 top-0 left-0 border-b">
                 <div className="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
                     <Link to="http://digital-mirage.ar/" className="flex items-center">
                         <img
@@ -36,20 +28,6 @@ const Header = () => {
                             alt="Digital Mirage Logo"
                         />
                     </Link>
-
-                    {/* Agregar el interruptor de modo oscuro aquí */}
-                    <Switch
-                        checked={darkMode}
-                        onChange={toggleDarkMode}
-                        className={`${darkMode ? 'bg-gray-700' : 'bg-gray-200'
-                            } relative inline-flex items-center h-6 rounded-full w-11 transition-colors focus:outline-none focus:ring-2 focus:ring-gray-200 dark:focus:ring-gray-700`}
-                    >
-                        <span className="sr-only">Modo oscuro</span>
-                        <span
-                            className={`${darkMode ? 'translate-x-6' : 'translate-x-1'
-                                } inline-block w-4 h-4 transform bg-white rounded-full transition-transform`}
-                        />
-                    </Switch>
 
                     <div className="flex md:order-2">
                         {login ? (
@@ -68,10 +46,7 @@ const Header = () => {
                         <button
                             onClick={toggleMenu}
                             type="button"
-                            className={`${darkMode
-                                ? 'text-gray-400 dark:text-gray-400'
-                                : 'text-gray-500 dark:text-gray-400'
-                                } inline-flex items-center p-2 w-10 h-10 justify-center text-sm rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:hover:bg-white-700 dark:focus:ring-gray-600`}
+                            className="text-gray-500 inline-flex items-center p-2 w-10 h-10 justify-center text-sm rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200"
                         >
                             <span className="sr-only">Abrir menú principal</span>
                             <svg
@@ -93,19 +68,14 @@ const Header = () => {
                     </div>
 
                     <div
-                        className={`items-center justify-between w-full md:flex md:w-auto md:order-1 ${menuOpen ? 'block' : 'hidden'
-                            }`}
+                        className={`items-center justify-between w-full md:flex md:w-auto md:order-1 ${menuOpen ? 'block' : 'hidden'}`}
                         id="navbar-sticky"
                     >
                         <ul className="flex flex-col p-4 md:p-0 mt-4 font-medium border border-gray-100 rounded-lg bg-white md:flex-row md:space-x-8 md:mt-0 md:border-0 md:bg-white dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700">
                             <li>
                                 <Link
                                     to="/"
-                                    className={`block py-2 pl-3 pr-4  hover:text-blue-500 ${darkMode ? 'text-blue-500 dark:md:text-white' : 'text-white'
-                                        } ${darkMode
-                                            ? 'bg-gray-700 dark:md:bg-gray-800'
-                                            : 'bg-gray-800'
-                                        } rounded md:bg-transparent md:text-blue-700 md:p-0`}
+                                    className="block py-2 pl-3 pr-4  hover:text-blue-500 text-white bg-gray-800 rounded md:bg-transparent md:text-blue-700 md:p-0"
                                     aria-current="page"
                                 >
                                     Inicio
@@ -114,11 +84,7 @@ const Header = () => {
                             <li>
                                 <Link
                                     to="/nosotros"
-                                    className={`block hover:text-blue-500 py-2 pl-3 pr-4 ${darkMode ? 'text-blue-500 dark:md:text-white' : 'text-white'
-                                        } ${darkMode
-                                            ? 'bg-white dark:bg-gray-800'
-                                            : 'bg-gray-800'
-                                        } rounded md:bg-transparent md:text-blue-700 md:p-0`}
+                                    className="block hover:text-blue-500 py-2 pl-3 pr-4 text-white bg-gray-800 rounded md:bg-transparent md:text-blue-700 md:p-0"
                                     aria-current="page"
                                 >
                                     Nosotros
@@ -127,11 +93,7 @@ const Header = () => {
                             <li>
                                 <Link
                                     to="/faq"
-                                    className={`block py-2 pl-3 pr-4  hover:text-blue-500 ${darkMode ? 'text-blue-500 dark:md:text-white' : 'text-white'
-                                        } ${darkMode
-                                            ? 'bg-white dark:bg-gray-800'
-                                            : 'bg-gray-800'
-                                        } rounded md:bg-transparent md:text-blue-700 md:p-0`}
+                                    className="block py-2 pl-3 pr-4  hover:text-blue-500 text-white bg-gray-800 rounded md:bg-transparent md:text-blue-700 md:p-0"
                                     aria-current="page"
                                 >
                                     FAQ
@@ -140,11 +102,7 @@ const Header = () => {
                             <li>
                                 <Link
                                     to="/contacto"
-                                    className={`block py-2 pl-3 pr-4  hover:text-blue-500 ${darkMode ? 'text-blue-500 dark:md:text-white' : 'text-white'
-                                        } ${darkMode
-                                            ? 'bg-white-700 dark:bg-gray-800'
-                                            : 'bg-white-700'
-                                        } rounded md:bg-transparent md:text-blue-700 md:p-0`}
+                                    className="block py-2 pl-3 pr-4  hover:text-blue-500 text-white bg-white-700 rounded md:bg-transparent md:text-blue-700 md:p-0"
                                     aria-current="page"
                                 >
                                     Contacto
